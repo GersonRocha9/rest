1caf84b189330d8b1b5b9ddf86bb394f4fbbc257
GersonRocha9 <gersonrocha9@gmail.com>
2021-05-23 15:53:41 -0300

    Fixando o bug dos links do Header e Footer

--- footer.php
+++ footer.php
@@ -1,13 +1,14 @@
 		<footer>
 			<nav class="nav-footer">
 				<ul>
-					<li class="current_page_item"><a href="index.html">Menu</a></li>
-					<li><a href="sobre.html">Sobre</a></li>
-					<li><a href="contato.html">Contato</a></li>
+					<li class="current_page_item"><a href="/">Menu</a></li>
+					<li><a href="/sobre">Sobre</a></li>
+					<li><a href="/contato">Contato</a></li>
 				</ul>
 			</nav>
 
-			<p>Rest © 2015. Alguns direitos reservados.</p>
+			<p>Rest © 2021. Alguns direitos reservados.</p>
+			<p>Desenvolvido por <a href="https://www.linkedin.com/in/gersonrocha9">Gerson Rocha</a> como projeto de conclusão do curso de "Wordpress como CMS" da <a href="https://www.origamid.com">Origamid</a></p>
 		</footer>
 
 		<!-- Footer Wordpress -->
